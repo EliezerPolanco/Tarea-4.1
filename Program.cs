35714734d34c75fb42667a1b8d84a6f78bb8145c
Eliezer Garcia <Eliezerp.garcia@hotmail.com>
2024-08-19 10:23:23 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+class Programa
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese su fecha de nacimiento en el formato (dd/mm/aaaa):");
+        string fechaNacimientoTexto = Console.ReadLine();
+
+        DateTime fechaNacimiento;
+        bool esFechaValida = DateTime.TryParseExact(fechaNacimientoTexto, "dd/MM/yyyy", null, DateTimeStyles.None, out fechaNacimiento);
+
+        if (esFechaValida)
+        {
+            string diaSemana = fechaNacimiento.ToString("dddd", new CultureInfo("es-ES"));
+
+            Console.WriteLine($"Naciste un día {diaSemana}.");
+        }
+        else
+        {
+            Console.WriteLine("La fecha ingresada no es válida. Asegúrate de utilizar el formato correcto (dd/mm/aaaa).");
+        }
+    }
+}
